56e5685d595be0f57ec9bc87d729228229eaca76
Jonathan Legrand <jonathan.a.legrand@inria.fr>
2025-05-24 12:15:43 +0200

    Comments and cleaning

--- generate_contributor_graph.py
+++ generate_contributor_graph.py
@@ -1,42 +1,45 @@
-# %%
-from pandas import DataFrame
+"""
+Generate a graph a communities. A community is made of contributors who
+share common concerns.
+We start by creating of graph of contributors. Communities are found
+by partioning the graph of contributors with Louvain algorithm.
+Finally, communities are represented as nodes in a graph, with edges
+representing the similarity of the communities' concerns.
+
+"""
 import numpy as np
-import matplotlib.pyplot as plt
 import pandas as pd
 import networkx as nx
 from itertools import combinations
 from datasets import load_dataset
 from pyvis.network import Network
-from scipy.spatial.distance import cosine, euclidean, chebyshev, jensenshannon
+from scipy.spatial.distance import jensenshannon
 
 rng = np.random.default_rng(seed=1234)
-# We only select a few columns to avoid OOM issues
+
+# We only select a subset of columns and samples to avoid OOM issues
 relevant_cols = ["contribution_id", "cluster_id", "cluster_title"]
 N_SAMPLES = 45000
 
-# %%
 ds = load_dataset("perspectiva-solution/embeddings-gdn-question-163")['train']
 df = ds.to_pandas()
-df = df.loc[df.cluster_id != -1, relevant_cols].sample(n=N_SAMPLES, random_state=1234) # Remove idea which were not clustered
+df = df.loc[df.cluster_id != -1, relevant_cols].sample(n=N_SAMPLES, random_state=1234) # Remove ideas which were not clustered
+n_clusters = df.cluster_title.nunique()
 
-# %%
-# TODO Probably refactor as object since we keep passing personas around
+# TODO Probably refactor these functions as methods
+# of an object since we keep passing personas around
 
-def sim_from_dist(vec_a, vec_b, dist=chebyshev):
+def sim_from_dist(vec_a, vec_b, dist=jensenshannon):
     return 1 / (1 + dist(vec_a, vec_b))
 
 def characterize_node(idx, personas, id_col="cluster_title"):
-    """Return common contributions of the community
-
-    Args:
-        idx (_type_): _description_
+    """Return common contributions of the community sorted by frequency of occurence
     """
     msk = df.contribution_id.isin(personas[idx])
     community_demands = df[msk].groupby(id_col)["cluster_title"].count().rename("cluster_count")
     return (community_demands / community_demands.sum()).sort_values(ascending=False)
 
     
-n_clusters = df.cluster_title.nunique()
 def node_to_vec(node, personas):
     char = characterize_node(node, personas, id_col="cluster_id")
     vec = np.zeros((n_clusters))
@@ -68,8 +71,7 @@ def find_bridge(node_pair, personas, topk=5):
     return bridge
 
 
-
-# %%
+# Create contributors graph
 G = nx.Graph()
 G.add_nodes_from(df.contribution_id) # A node is a contribution
 
@@ -82,24 +84,24 @@ for title, subdf in df.groupby("cluster_title"):
         else:
             G.add_edge(*edge, weight=1)
 
-# %%
+# Find a partition of the graph
+# Personas are defined as "big enough" (>threshold) communities
 partition = nx.community.louvain_communities(
     G, weight="weight", resolution=1, seed=2025
 )
-
-# %%
-# Big partitions could be personas
 personas = []
-threshold = 10#len(df) // 500
+threshold = 10
 for node_set in partition:
     if len(node_set) >= threshold:
         personas.append(node_set)
 
 
-# %%
+# Set scaling factors for edge and node display
 EDGE_SCALE_FACTOR = 0.1
+max_comm_size = max((len(p) for p in personas))
+size_factor = 100 / max_comm_size
 
-# Graph of Louvain communities
+# Create graph of Louvain communities
 H = nx.Graph()
 H.add_nodes_from(range(len(personas)), borderWidth=0.5)
 n_tests = len(tuple(combinations(H.nodes, 2)))
@@ -129,15 +131,12 @@ for node_pair in combinations(H.nodes, 2):
     )
 
 
-# %%
-max_comm_size = max((len(p) for p in personas))
-size_factor = 100 / max_comm_size
 for community in H.nodes:
     community_demands = characterize_node(community, personas)
     H.nodes[community]["title"] = make_title(community_demands)
     H.nodes[community]["size"] = len(personas[community]) * size_factor
 
-# %%
+# Plot graph of communities with Pyviz
 nt = Network("500px", "1000px")
 nt.force_atlas_2based()
 nt.from_nx(H)
